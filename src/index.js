c682c4e233c048a109e49ac158ae2079520f9145
Rohithmittu <103620418+Rohithmittu@users.noreply.github.com>
2024-04-16 23:19:17 +0530

    learned why each and every line do in backend,how to write errorhandler,responsehandler,asynchandlers

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
-// require('dotenv').config({path: "./env"})
-import dotenv from "dotenv"
+// require('dotenv').config({path: "./env"})  //instead use the down part
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
-
+import app from "./app.js";
 
 dotenv.config({
-    path: "./env"
-})
+  path: "./env",
+});
 
+connectDB()
+  .then(() => {
+    app.on("errror", (error) => {
+        console.log("ERRR: ", error);
+        throw error
+    })
 
-connectDB();
-
-
-
-
-
-
+    app.listen(process.env.PORT || 4000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+  })
+  .catch((e) => {
+    console.log("MONGO DB connection failed!!!", e);
+  });
