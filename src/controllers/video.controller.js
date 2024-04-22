1b5aecae82cc53c56fc693dd71bb3100664e9e71
Rohithmittu <103620418+Rohithmittu@users.noreply.github.com>
2024-04-22 20:12:00 +0530

    fetching all-videos based on query ,page,limit

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -8,7 +8,85 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const getAllVideos = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
-  //TODO: get all videos based on query, sort, pagination
+  // console.log("Query Parameters:", req.query);
+
+  // TODO: get all videos based on query, sort, pagination
+
+  const videoAggregate = Video.aggregate([
+
+    /// so don't just wait this because it will start fetching the information right away but we need
+    //  to add page and limit so after adding it we need to fetch ,
+    // so wait untill ((( aggregatePaginate ))) with options
+
+    {
+      $search: {     // inorder to search we set up the MOngo atlas to indexes as search-videos and add title and description for it so that we enable the search feature
+        index: "search-videos",
+        text: {
+          query: query,
+          path: ["title", "description"],
+        },
+      },
+    },
+
+    {
+      $match: {
+        $and: [
+          userId ? { owner: new mongoose.Types.ObjectId(userId) } : {},
+          { isPublished: true },
+        ],
+      },
+    },
+
+    {
+      $sort:
+        sortBy && sortType
+          ? {
+              [sortBy]: sortType === "asc" ? 1 : -1,
+            }
+          : {
+              createdAt: -1,
+            },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "ownerDetails",
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              "avatar.url": 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $unwind: "$ownerDetails",
+    },
+  ]);
+
+  // console.log("Aggregation Pipeline:", JSON.stringify(videoAggregate, null, 2));
+
+  const options = {
+    page: parseInt(page, 10),
+    limit: parseInt(limit, 10),
+  };
+
+  try {
+    const video = await Video.aggregatePaginate(videoAggregate, options);
+    // console.log("HEllloo Videossssss", video);
+    return res
+      .status(200)
+      .json(new ApiResponse(200, video, "Videos fetched successfully"));
+  } catch (error) {
+    console.error("Aggregation Error:", error);
+    return res
+      .status(500)
+      .json(new ApiResponse(500, null, "An error occurred"));
+  }
 });
 
 const publishAVideo = asyncHandler(async (req, res) => {
@@ -20,10 +98,10 @@ const publishAVideo = asyncHandler(async (req, res) => {
 
   const videoLocalPath = req.files?.videoFile[0]?.path;
 
-//   console.log(videoLocalPath);
-  
+  //   console.log(videoLocalPath);
+
   const thumbnailLocalPth = req.files?.thumbnail[0]?.path;
-//   console.log(thumbnailLocalPth);
+  //   console.log(thumbnailLocalPth);
 
   if (!videoLocalPath) {
     throw new ApiError(400, "Video file is required");
@@ -33,10 +111,10 @@ const publishAVideo = asyncHandler(async (req, res) => {
   }
 
   const videoFile = await uploadOnCloudinary(videoLocalPath);
-//   console.log(videoFile);
-  
+  //   console.log(videoFile);
+
   const thumbnail = await uploadOnCloudinary(thumbnailLocalPth);
-//   console.log(thumbnail);
+  //   console.log(thumbnail);
 
   const uploadVideo = await Video.create({
     title,
@@ -62,6 +140,7 @@ const publishAVideo = asyncHandler(async (req, res) => {
 const getVideoById = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   //TODO: get video by id
+
 });
 
 const updateVideo = asyncHandler(async (req, res) => {
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,8 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-
-
 const videoSchema = new Schema(
   {
     videoFile: {
@@ -43,11 +41,4 @@ const videoSchema = new Schema(
 
 videoSchema.plugin(mongooseAggregatePaginate);
 
-
-
-
-
-
-
 export const Video = mongoose.model("Video", videoSchema);
-
